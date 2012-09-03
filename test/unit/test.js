ba96dee33ef31804df677723f5d17bea9d15a2c7
Philip Sampaio <philip.sampaio@gmail.com>
2012-09-03 00:56:00 -0300

    Moving test code to test file

--- test/unit/test.js
+++ test/unit/test.js
@@ -1,5 +1,10 @@
 $(document).ready(function(){
   test("basic configuration", function() {
+    Sprition.positions = [ '-27px -218px', '-99px -220px', '-187px -218px', '-262px -220px' ];
+    Sprition.maxSprites = 3;
+    Sprition.interval = 500;
+    Sprition.objId = 'animation';
+    Sprition.run();
     ok(true)
   });
 
